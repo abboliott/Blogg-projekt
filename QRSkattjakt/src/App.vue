58b3efec24151487e737865e3b52dfe6cc5f52d8
abb21timsjo <tim.sjostrand@abbgymnasiet.se>
2024-04-18 14:16:55 +0200

    vue prodject started

---
+++ QRSkattjakt/src/App.vue
@@ -0,0 +1,35 @@
+
+
+<template>
+  <div>
+    <div>
+      <img :src="imageUrl" alt="Image" />
+    </div>
+    <div>
+      <button @click="downloadImage">Download Image</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import image from './assets/shocked-surprised.gif';
+export default {
+  data() {
+    return {
+      imageUrl: image 
+    };
+  },
+  methods: {
+    downloadImage() {
+      // Create a new anchor element
+      const link = document.createElement('a');
+      // Set the href attribute to the image source
+      link.href = this.imageUrl;
+      // Set the download attribute to specify the file name
+      link.download = 'image.jpg'; // Change 'image.jpg' to your desired file name
+      // Simulate a click on the anchor element
+      link.click();
+    }
+  }
+};
+</script>
